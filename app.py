261daed88d890c4cf7c3f4f561957126d1472068
ellapot <michaellamarie.amoren.mnl@eac.edu.ph>
2025-09-15 18:58:29 +0800

    on render

--- app.py
+++ app.py
@@ -271,4 +271,5 @@ def switch_filipino_model(model_name: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))  
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=False)
